8093847f3212347da1cc96ed28fb2e62f5fe3691
Thomas-DALLA-FRANCESCA <thomas.dalla-francesca@orange.fr>
2015-11-26 13:53:15 +0100

    Mise en place des commentaires

--- src/scriptmodifier/ScriptModifier.java
+++ src/scriptmodifier/ScriptModifier.java
@@ -26,21 +26,27 @@ public final class ScriptModifier extends Application {
  
     @Override
     public void start(final Stage stage) {
+        // Titre de la fenetre principale
         stage.setTitle("ScriptModifier");
  
+        // Déclaration des sélecteurs
         final FileChooser fileChooser = new FileChooser();
         final DirectoryChooser directoryChooser = new DirectoryChooser();
  
+        // Déclaration des boutons
         final Button openButton = new Button("Rechercher...");
         final Button openMultipleButton = new Button("Rechercher...");
         final Button operation = new Button("Modifier");
         
+        // Déclaration du champ d'affichage du fichier d'entrée sélectionné
         final Label fichierAModifier = new Label("Fichier à modifier:");
         final TextField champFichierAModifier = new TextField ();
         
+        // Déclaration du champ d'affichage du répertoire de destination sélectionné
         final Label fichierModifie = new Label("Fichier modifié:");
         final TextField champFichierModifie = new TextField ();
 
+        // Action à réaliser pour le bouton de sélection de fichier
         openButton.setOnAction(
             (final ActionEvent e) -> {
                 configureFileChooser(fileChooser);
@@ -49,20 +55,25 @@ public final class ScriptModifier extends Application {
                     champFichierAModifier.setText(file.getAbsolutePath());
                 }
         });
+        
+        // Action à réaliser pour le bouton de sélection de répertoire
         openMultipleButton.setOnAction(
             (final ActionEvent e) -> {
                 configureDirectoryChooser(directoryChooser);
                 champFichierModifie.setText(directoryChooser.showDialog(stage).toString());
         });
 
+        // Action à réaliser pour le bouton déclenchant l'opération
         operation.setOnAction(
             (final ActionEvent e) -> {
                 String nomFichier =  (champFichierAModifier.getText().substring(0, champFichierAModifier.getText().length()-4)) + "_MODIFIE.sql";
                 operation(champFichierAModifier.getText(), nomFichier);
         });
         
+        // Déclaration du gridPane permettant l'agencement des différents éléments graphiques
         final GridPane inputGridPane = new GridPane();
  
+        // Emplacements des éléments graphiques
         GridPane.setConstraints(fichierAModifier, 0, 0);
         GridPane.setConstraints(champFichierAModifier, 1, 0);
         GridPane.setConstraints(fichierModifie, 0, 1);
@@ -70,22 +81,33 @@ public final class ScriptModifier extends Application {
         GridPane.setConstraints(openButton, 2, 0);
         GridPane.setConstraints(openMultipleButton, 2, 1);
         GridPane.setConstraints(operation, 1, 2);
+        
+        // Déclaration de la taille du gridPane
         inputGridPane.setHgap(6);
         inputGridPane.setVgap(6);
+        
+        // Ajout des éléments au gridPane
         inputGridPane.getChildren().addAll(fichierAModifier, champFichierAModifier, fichierModifie, champFichierModifie, openButton, openMultipleButton, operation);
         
+        // Déclaration du Pane contenant le gridPane
         final Pane rootGroup = new VBox(12);
         rootGroup.getChildren().addAll(inputGridPane);
         rootGroup.setPadding(new Insets(12, 12, 12, 12));
  
+        // Méthode d'affichage des éléments graphiques
         stage.setScene(new Scene(rootGroup));
         stage.show();
     }
  
+    // Méthode de lancement de l'application
     public static void main(String[] args) {
         Application.launch(args);
     }
    
+    /*
+    Méthode de configuration du sélecteur de fichier
+    @param : sélecteur de fichier
+    */
     private static void configureFileChooser(final FileChooser fileChooser){                           
         fileChooser.setTitle("Recherche Fichier");
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
@@ -95,6 +117,10 @@ public final class ScriptModifier extends Application {
         ); 
     }
     
+    /*
+    Méthode de configuration du sélecteur de répertoire
+    @ param : Sélecteur de répertoire
+    */
     private static void configureDirectoryChooser(final DirectoryChooser directoryChooser){                           
         directoryChooser.setTitle("Emplacement");
         directoryChooser.setInitialDirectory(
@@ -102,6 +128,11 @@ public final class ScriptModifier extends Application {
         ); 
     }
     
+    /*
+    Méthode effectuant la transformation du fichier texte
+    @ param fichier : Nom du fichier à traiter
+    @param fichierSortie : Nom du fichier traité créé
+    */
     private static void operation(String fichier, String fichierSortie){
         try{
             InputStream ips=new FileInputStream(fichier); 
